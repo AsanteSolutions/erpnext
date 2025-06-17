76d53c14b9a110747bd4bbcff62c979d31d32133
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2025-06-18 01:04:51 +0530

    fix: update validation and test cases

--- erpnext/setup/doctype/company/company.py
+++ erpnext/setup/doctype/company/company.py
@@ -16,6 +16,7 @@ from frappe.utils.nestedset import NestedSet, rebuild_tree
 
 from erpnext.accounts.doctype.account.account import get_account_currency
 from erpnext.setup.setup_wizard.operations.taxes_setup import setup_taxes_and_charges
+from erpnext.stock.utils import check_pending_reposting
 
 
 class Company(NestedSet):
@@ -141,6 +142,7 @@ class Company(NestedSet):
 		return exists
 
 	def validate(self):
+		old_doc = self.get_doc_before_save()
 		self.update_default_account = False
 		if self.is_new():
 			self.update_default_account = True
@@ -156,7 +158,11 @@ class Company(NestedSet):
 		self.check_parent_changed()
 		self.set_chart_of_accounts()
 		self.validate_parent_company()
+<<<<<<< HEAD
 		self.set_reporting_currency()
+=======
+		self.validate_pending_reposts(old_doc)
+>>>>>>> 4be1172bda (fix: update validation and test cases)
 
 	def validate_abbr(self):
 		if not self.abbr:
@@ -501,6 +507,10 @@ class Company(NestedSet):
 				"Company", self.parent_company, ["reporting_currency"]
 			)
 			self.reporting_currency = parent_reporting_currency
+	def validate_pending_reposts(self, old_doc):
+		if old_doc.accounts_frozen_till_date != self.accounts_frozen_till_date:
+			if self.accounts_frozen_till_date:
+				check_pending_reposting(self.accounts_frozen_till_date)
 
 	def set_default_accounts(self):
 		default_accounts = {
